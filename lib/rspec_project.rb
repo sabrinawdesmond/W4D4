d2327a5ce4e73060857dbb03c98df0f3a7b0421a
Justin <jcdiner@gmail.com>
2023-02-02 15:00:19 -0500

    progress on my_uniq

--- lib/rspec_project.rb
+++ lib/rspec_project.rb
@@ -1,5 +1,15 @@
-def remove_dups(arr)
+arr = [1, 2, 3, 1]
+
+def my_uniq(arr)
+	final = []
+
 	if !arr.is_a? (Array)
 		raise "must accept an array"
 	end
+	arr.each do |ele|
+		if !final.include?(ele)
+			final << ele
+		end
+	end
+	final 
 end
--- spec/rspec_project_spec.rb
+++ spec/rspec_project_spec.rb
@@ -1,15 +1,26 @@
-require "rspec"
-require "rspec_project.rb"
+require "rspec_project"
 
-describe "#remove_dups" do
+describe "#my_uniq" do
   subject(:arr) { Array.new(4, "A") }
 
-  context "properly accepts an argument" do
-    it "accepts an array as an argument" do
-      expect(arr).to be_an_instance_of(Array)
-    end
-    it "raises error if no array is given" do
-      expect{ remove_dups(2) }.to raise_error("must accept an array")
-    end
-  end
+  	context "properly accepts an argument" do
+		it "accepts an array as an argument" do
+		expect(arr).to be_an_instance_of(Array)
+		end
+
+		it "raises error if no array is given" do
+		expect{ my_uniq(2) }.to raise_error("must accept an array")
+		end
+
+		it "should return a new array containing unique elements from the original array" do
+			expect( my_uniq([1, 2, 3, 1])).to eq([1, 2, 3])
+			expect( my_uniq([2, 4, 2, 4, 6])).to eq([2, 4, 6])
+			expect( my_uniq(arr)).to eq(["A"])
+			
+		end
+
+		it "should not use built in Array#uniq" do
+			expect(arr).to_not receive(:uniq)
+		end
+	end
 end
