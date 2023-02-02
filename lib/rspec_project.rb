c60738f9757461fd941a7cceafe9ecb49e580c59
Sabrina Desmond <sabrinawdesmond@gmail.com>
2023-02-02 14:42:50 -0500

    more specs

--- lib/rspec_project.rb
+++ lib/rspec_project.rb
@@ -1,3 +1,5 @@
 def remove_dups(arr)
-	puts arr
+	if !arr.is_a? (Array)
+		raise "must accept an array"
+	end
 end
--- spec/rspec_project_spec.rb
+++ spec/rspec_project_spec.rb
@@ -1,11 +1,15 @@
-require 'rspec'
-require 'rspec_project.rb'
+require "rspec"
+require "rspec_project.rb"
 
-describe '#remove_dups' do 
-	it 'accepts an array as an argument' do 
-		expect {remove_dups([])}.to raise_error do 
-			it 'raises error if no array is given' do 
-			end
-		end
-	end
+describe "#remove_dups" do
+  subject(:arr) { Array.new(4, "A") }
+
+  context "properly accepts an argument" do
+    it "accepts an array as an argument" do
+      expect(arr).to be_an_instance_of(Array)
+    end
+    it "raises error if no array is given" do
+      expect{ remove_dups(2) }.to raise_error("must accept an array")
+    end
+  end
 end
